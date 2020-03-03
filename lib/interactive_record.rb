f7a4763bf9d226085e4e79867b4b6b2af59f19e8
Taryn Johnson <taryn.johnson919@gmail.com>
2020-03-03 00:41:43 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -47,11 +47,7 @@ end
   values.join(", ")
   end
   
-  def save
-  DB[:conn].execute("INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (?)", [values_for_insert])
- 
-  @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
-  end
+  
 
 
   def self.find_by_name(name)
