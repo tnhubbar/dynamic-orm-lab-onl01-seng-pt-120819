35802905aa4160a0f088d707bc9954b1dfae1bd2
Taryn Johnson <taryn.johnson919@gmail.com>
2020-03-03 00:38:25 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -46,6 +46,13 @@ end
   end
   values.join(", ")
   end
+  
+  def save
+  DB[:conn].execute("INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (?)", [values_for_insert])
+ 
+  @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
+
 
 
 
