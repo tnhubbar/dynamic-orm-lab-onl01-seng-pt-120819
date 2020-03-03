26b8a91283211cda3653937ddb418650e5bc5289
Taryn Johnson <taryn.johnson919@gmail.com>
2020-03-03 00:36:19 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -3,4 +3,25 @@ require 'active_support/inflector'
 
 class InteractiveRecord
   
+    def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+  sql = "PRAGMA table_info('#{table_name}')"
+  table_info = DB[:conn].execute(sql)
+  column_names = []
+ 
+  table_info.each do |column|
+    column_names << column["name"]
+    end
+ 
+  column_names.compact
+  end
+  
+  self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
+  end
+
+  
 end
