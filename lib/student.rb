9f1bd2c04ca6dd60f27253e25cca9198a6bde4d3
Taryn Johnson <taryn.johnson919@gmail.com>
2020-03-03 00:44:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,7 +8,10 @@ self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
   end
   
-  
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
+  end
 
 
 
