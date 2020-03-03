27be55d69d71eed3dbe206f8339ad54f5ed85266
Taryn Johnson <taryn.johnson919@gmail.com>
2020-03-03 00:36:30 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -22,6 +22,13 @@ class InteractiveRecord
   self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
   end
+  
+  def initialize(options={})
+  options.each do |property, value|
+    self.send("#{property}=", value)
+  end
+end
+
 
   
 end
