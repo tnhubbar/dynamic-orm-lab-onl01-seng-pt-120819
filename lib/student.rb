e7914604823bf98179eecb7239f78e8e5c619247
Taryn Johnson <taryn.johnson919@gmail.com>
2020-03-03 00:45:06 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -4,4 +4,20 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
+self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
+  end
+  
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
+  end
+
+def self.find_by(attribute_hash)
+    value = attribute_hash.values.first
+    formatted_value = value.class == Fixnum ? value : "'#{value}'"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{formatted_value}"
+    DB[:conn].execute(sql)
+  end
+
 end
